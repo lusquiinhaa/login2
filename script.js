4b3f50e0e1d6384ccc770feec5391bf15835415f
Lucas Lima <101532817+lusquiinhaa@users.noreply.github.com>
2024-09-09 16:18:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function(){
+    // Defina a senha correta
+    const correctPassword = "pbsapmn";
+    // Defina a URL para redirecionamento
+    const redirectUrl = "photos/dist/index.html"; // Corrigido o caminho
+
+    $("#login").submit(function(e){
+        e.preventDefault();
+        
+        // Obtenha o valor da senha
+        const enteredPassword = $(".pass").val();
+
+        if (enteredPassword === correctPassword) {
+            // Redirecionar para a nova página
+            window.location.href = redirectUrl;
+        } else {
+            alert("Senha incorreta. Tente novamente.");
+            $(".login").addClass("init-shake");
+            setTimeout(function(){
+                $(".login").removeClass("init-shake");
+            }, 1000);
+        }
+    });
+    
+    $(".pass").on("keypress", function(){
+        $(".arrow").css("opacity", "1");
+    });
+});
